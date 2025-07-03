d1ad2dd9a30be61e723f5c10891d1f3085b4ae4c
Ghaida02a <ggmskaj@gmail.com>
2025-07-03 14:21:09 +0400

    Edited Object.txt and added Coffee Machine file

---
+++ CoffeeMachine.java
@@ -0,0 +1,33 @@
+public class CoffeeMachine {
+    
+	
+
+
+    public static void main(String[] args) {
+        Door door = new Door();
+		
+		door.isDoorOpen = true;
+		door.isDoorLocked = true;
+		
+		door.openingDoor();
+		door.lockingDoor();
+	}
+	
+	public void openingDoor(){
+		if (isDoorOpen){
+			System.out.println("The door is open");
+		}
+		else {
+			System.out.println("The door is closed");
+		}
+	}
+        
+	public void lockingDoor(){
+		if (isDoorOpen == false){
+			System.out.println("The door is locked");
+		}
+		else {
+			System.out.println("The door is unlocked");
+		}
+	}
+}
