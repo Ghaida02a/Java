f02f07764a65aafa794c9f01cb98f024466ab364
Ghaida02a <ggmskaj@gmail.com>
2025-07-06 08:44:36 +0400

    Edited Object.txt

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,33 +1,19 @@
 public class CoffeeMachine {
-    
+    boolean isPowerOn;
+	int waterLevel;
+	int Temperature;
+	
 	
 
 
     public static void main(String[] args) {
-        Door door = new Door();
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
 		
-		door.isDoorOpen = true;
-		door.isDoorLocked = true;
 		
-		door.openingDoor();
-		door.lockingDoor();
 	}
 	
-	public void openingDoor(){
-		if (isDoorOpen){
-			System.out.println("The door is open");
-		}
-		else {
-			System.out.println("The door is closed");
-		}
+	public void powerOnOrOff(){
+		
 	}
-        
-	public void lockingDoor(){
-		if (isDoorOpen == false){
-			System.out.println("The door is locked");
-		}
-		else {
-			System.out.println("The door is unlocked");
-		}
-	}
+	
 }
