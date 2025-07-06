4433aa67b14cd890e2c3adf48007e0c600ce58b6
Ghaida02a <ggmskaj@gmail.com>
2025-07-06 08:47:15 +0400

    Coffee Machine Object Code Added

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,19 +1,40 @@
 public class CoffeeMachine {
-    boolean isPowerOn;
-	int waterLevel;
-	int Temperature;
-	
-	
-
+    boolean isCoffeeMachineOn;
+    int waterLevel;
 
     public static void main(String[] args) {
         CoffeeMachine coffeeMachine = new CoffeeMachine();
+        
+		coffeeMachine.isCoffeeMachineOn = true;
+        coffeeMachine.waterLevel = 400;
 		
-		
-	}
-	
-	public void powerOnOrOff(){
-		
-	}
-	
+        coffeeMachine.turnOnOrOff();
+        coffeeMachine.checkWaterLevel();
+    }
+
+    public void turnOnOrOff() {
+        if (isCoffeeMachineOn) {
+            System.out.println("Coffee Machine is on");
+        }
+		else {
+            System.out.println("Coffee Machine is off");
+        }
+    }
+
+    public void checkWaterLevel() {
+        if (isCoffeeMachineOn) {
+            if (waterLevel > 150) {
+                System.out.println("Water level is sufficient: " + waterLevel + " ml");
+            }
+			else if (waterLevel > 0) {
+                System.out.println("Water level is low: " + waterLevel + " ml. \n refill it.");
+            }
+			else {
+                System.out.println("Water level is empty. Please refill.");
+            }
+        }
+		else {
+            System.out.println("Can not check water level because Coffee Machine is off");
+        }
+    }
 }
